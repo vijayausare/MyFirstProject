c6f39616bf49b7c207306304aae421da5ef8f4cb
Vijay <vijayausare0007@gmail.com>
2023-07-04 10:16:53 +0530

    uploaded project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    NavBarComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
